82f67ef6fff5673b315a285070c55b5f9309ebe1
Andreas Müller <taori@users.noreply.github.com>
2024-08-07 01:46:01 +0200

    added test for coverlet to work

---
+++ tests/Amusoft.DotnetNew.Tests.IntegrationTests/CoverletTest.cs
@@ -0,0 +1,15 @@
+using Shouldly;
+
+namespace Amusoft.DotnetNew.Tests.IntegrationTests;
+
+public class CoverletTest
+{
+    [Fact]
+    public void DirectoryExists()
+    {
+        using (var dir = new TempDirectory())
+        {
+            Directory.Exists(dir.Path).ShouldBeTrue();
+        }
+    }
+}
