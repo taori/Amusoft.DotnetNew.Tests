2db89601dd7900b0178164f3bf865d543ef03552
Andreas Müller <taori@users.noreply.github.com>
2024-08-16 20:20:20 +0200

    increase of duration

--- tests/Amusoft.DotnetNew.Tests.UnitTests/Tests/DotnetNewTests.cs
+++ tests/Amusoft.DotnetNew.Tests.UnitTests/Tests/DotnetNewTests.cs
@@ -17,7 +17,7 @@ namespace Amusoft.DotnetNew.Tests.UnitTests.Tests;
 
 public class DotnetNewTests : TestBase
 {
-	[Theory(Timeout = 30000)]
+	[Theory(Timeout = 60000)]
 	[InlineData("Project1", "gitUser", "authorname")]
 	[InlineData("Project2", "gitUser", "authorname")]
 	private async Task ScaffoldRepo(string projectName, string gitUser, string author)
