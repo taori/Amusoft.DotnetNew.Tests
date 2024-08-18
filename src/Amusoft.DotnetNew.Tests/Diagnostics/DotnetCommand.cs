3e2d3eb50762274c0df3ce60e4abc2577917058f
Andreas Müller <taori@users.noreply.github.com>
2024-08-18 02:32:45 +0200

    Coverage increase

--- src/Amusoft.DotnetNew.Tests/Diagnostics/DotnetCommand.cs
+++ src/Amusoft.DotnetNew.Tests/Diagnostics/DotnetCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
 using Amusoft.DotnetNew.Tests.Interfaces;
 using Amusoft.DotnetNew.Tests.Templating;
 
@@ -6,6 +7,7 @@ namespace Amusoft.DotnetNew.Tests.Diagnostics;
 
 internal class DotnetCommand : ICommandInvocation
 {
+	[ExcludeFromCodeCoverage]
 	public string Command { get; }
 
 	public DotnetCommand(string command)
--- tests/Amusoft.DotnetNew.Tests.UnitTests/Tests/CommandResultTests.cs
+++ tests/Amusoft.DotnetNew.Tests.UnitTests/Tests/CommandResultTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading.Tasks;
 using Amusoft.DotnetNew.Tests.Diagnostics;
 using VerifyXunit;
@@ -10,9 +11,20 @@ namespace Amusoft.DotnetNew.Tests.UnitTests.Tests;
 public class CommandResultTests
 {
 	[Fact]
-	private async Task RenderCheck()
+	private async Task VerifyCommandResult()
 	{
 		var item = new CommandResult(0, "some output", "some error", true, TimeSpan.Zero);
-		await Verifier.Verify(item);
+		var sb = new StringBuilder();
+		item.Print(sb);
+		await Verifier.Verify(sb.ToString());
+	}
+
+	[Fact]
+	private async Task VerifyDotnetCommand()
+	{
+		var dotnetCommand = new DotnetCommand("wasd");
+		var sb = new StringBuilder();
+		dotnetCommand.Print(sb);
+		await Verifier.Verify(sb.ToString());
 	}
 }
